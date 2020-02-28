a12b9af1afe3f087d436033b91c0b6e4c4897531
Drew Mares <dmares01@hamline.edu>
2020-02-27 21:10:40 -0600

    Fixed error of planes not being appended to list. Found issue was submission time was being read as a string not an int

--- planeSchedule.py
+++ planeSchedule.py
@@ -4,7 +4,6 @@ import takeoffRequest
 def create_schedule(plane_list, current_time):
     current_schedule = []
     for plane in plane_list:
-        if plane.get_submission_time() == current_time:
+        if int(current_time) == int(plane.get_submission_time()):
             current_schedule.append(plane)
-
     return current_schedule
