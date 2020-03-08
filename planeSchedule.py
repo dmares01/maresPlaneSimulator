f965c409c256e06283be01def86b97fe0e77dfea
Drew Mares <dmares01@hamline.edu>
2020-03-07 19:57:22 -0600

    Had to modify function to take initial list, as well as current list to be appended too. spent many hours trying to figure out where I got myself into a never terminating loop. Founud out I was appending an empty list each time, and then calling the function again withthe new appended list on the end

--- planeSchedule.py
+++ planeSchedule.py
@@ -1,9 +1,23 @@
 import takeoffRequest
 
 
-def create_schedule(plane_list, current_time):
-    current_schedule = []
+def create_schedule(plane_list, current_time, current_queue):
+    # current_schedule = []
+    current_schedule = current_queue.copy()
     for plane in plane_list:
+        # print("in for loop")
         if int(current_time) == int(plane.get_submission_time()):
-            current_schedule.append(plane)
-    return current_schedule
+            x = get_index(current_schedule, plane)
+            print(x)
+            current_schedule.insert(x, plane)
+            # print(current_schedule)
+    if len(current_schedule) != 0:
+        return current_schedule
+
+
+def get_index(plane_schedule, plane):
+    for scheduled_plane in range(len(plane_schedule)):
+        if plane.get_takeoff_time() < plane_schedule[scheduled_plane].get_takeoff_time():
+            index = scheduled_plane
+            return index
+    return len(plane_schedule)
