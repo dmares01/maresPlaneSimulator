c71dca4797fb1e054f594e2d69bbfc7949a61cd4
Drew Mares <dmares01@hamline.edu>
2020-03-07 19:55:53 -0600

    Made it return the latest scheduled take off time to use while iterating

--- inputPlaneFile.py
+++ inputPlaneFile.py
@@ -6,9 +6,11 @@ from takeoffRequest import Requests
 
 def create_plane_list(list_of_planes):
     plane_input = []
-
+    largest_timer = 0
     for requests in list_of_planes:
         input_request = requests.split(",")
-        new_request = Requests(input_request[0], input_request[1], input_request[2], input_request[3])
+        new_request = Requests(input_request[0], int(input_request[1]), int(input_request[2]), int(input_request[3]))
+        if new_request.get_submission_time() >= largest_timer:
+            largest_timer = new_request.get_submission_time()
         plane_input.append(new_request)
-    return plane_input
+    return plane_input, int(largest_timer) * 2
