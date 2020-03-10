0bbe0e36b0cca3af1fe7dd3d0b50341a70f48ece
Drew Mares <dmares01@hamline.edu>
2020-03-09 23:05:29 -0500

    Fixed file to properly remove planes from queue. Spent many hours trying to figure out why the program was crashing when trying to remove plane. Was using planes from the wrong list

--- planeSchedule.py
+++ planeSchedule.py
@@ -2,22 +2,43 @@ import takeoffRequest
 
 
 def create_schedule(plane_list, current_time, current_queue):
-    # current_schedule = []
     current_schedule = current_queue.copy()
     for plane in plane_list:
-        # print("in for loop")
         if int(current_time) == int(plane.get_submission_time()):
-            x = get_index(current_schedule, plane)
-            print(x)
+            x = get_index_for_insert(current_schedule, plane)
             current_schedule.insert(x, plane)
-            # print(current_schedule)
+
+        if remove_plane(current_time, plane, current_schedule):
+            plane_to_remove = get_index(current_schedule, plane)
+            del current_schedule[plane_to_remove]
+            # print("removed ", plane.get_name())
+            
     if len(current_schedule) != 0:
         return current_schedule
 
 
-def get_index(plane_schedule, plane):
+def get_index_for_insert(plane_schedule, plane):
     for scheduled_plane in range(len(plane_schedule)):
         if plane.get_takeoff_time() < plane_schedule[scheduled_plane].get_takeoff_time():
             index = scheduled_plane
             return index
     return len(plane_schedule)
+
+
+def remove_plane(time, plane, list_to_check):
+    if time > (plane.get_takeoff_time() + plane.get_takeoff_duration()):
+        if plane in list_to_check:
+            return True
+
+
+def get_index(list_of_planes, plane):
+    for compare_plane in range(len(list_of_planes)):
+        if plane.get_name() == list_of_planes[compare_plane].get_name():
+            return compare_plane
+    print("\n\ngonna crash")
+    return 100
+
+
+def print_all_planes(all_planes):
+    for plane in all_planes:
+        print(plane.get_name())
