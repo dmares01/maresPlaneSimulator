9bfef80e35eb5ef7ac5e8a4197fa53c109a09be5
Drew Mares <dmares01@hamline.edu>
2020-03-12 11:40:54 -0500

    Fixed schedule to properly add planes and change takeoff times according to only one takeoff at a time

--- planeSchedule.py
+++ planeSchedule.py
@@ -1,20 +1,35 @@
-import takeoffRequest
-
-
 def create_schedule(plane_list, current_time, current_queue):
     current_schedule = current_queue.copy()
+    flag = False
     for plane in plane_list:
         if int(current_time) == int(plane.get_submission_time()):
             x = get_index_for_insert(current_schedule, plane)
             current_schedule.insert(x, plane)
+            make_takeoff_schedule(current_schedule, x)
 
         if remove_plane(current_time, plane, current_schedule):
             plane_to_remove = get_index(current_schedule, plane)
             del current_schedule[plane_to_remove]
+            flag = True
             # print("removed ", plane.get_name())
-            
-    if len(current_schedule) != 0:
+
+    if len(current_schedule) != 0 or flag is True:
         return current_schedule
+
+
+def make_takeoff_schedule(plane_list, index_of_new_plane):
+    # print("The index is", index_of_new_plane)
+    # print("The length is", len(plane_list))
+    next_takeoff_time = plane_list[index_of_new_plane - 1].get_actual_takeoff()
+
+    for plane in range(len(plane_list)):
+        # print("Next takeoff time is ", next_takeoff_time)
+        if plane_list[plane].get_actual_takeoff() > next_takeoff_time or plane < index_of_new_plane:
+            next_takeoff_time = plane_list[plane].get_actual_takeoff() + plane_list[plane].get_takeoff_duration()
+            continue
+        else:
+            plane_list[plane].set_actual_takeoff(next_takeoff_time)
+            next_takeoff_time = plane_list[plane].get_actual_takeoff() + plane_list[plane].get_takeoff_duration()
 
 
 def get_index_for_insert(plane_schedule, plane):
@@ -26,7 +41,7 @@ def get_index_for_insert(plane_schedule, plane):
 
 
 def remove_plane(time, plane, list_to_check):
-    if time > (plane.get_takeoff_time() + plane.get_takeoff_duration()):
+    if time > plane.get_actual_takeoff() + plane.get_takeoff_duration() > 0:
         if plane in list_to_check:
             return True
 
