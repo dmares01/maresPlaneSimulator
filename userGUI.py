075053da7ced713dba6768d26d0b00bebeba1cae
Drew Mares <dmares01@hamline.edu>
2021-02-18 17:41:41 -0600

    Modified file to open the window allowing the user to exit the program or select a file instead of directly calling the file browser function

--- userGUI.py
+++ userGUI.py
@@ -1,34 +1,36 @@
 import tkinter as tk
 from tkinter import filedialog
+import sys
 
 def browse_files():
     filename = filedialog.askopenfilename(initialdir = "/", title = "Select a File", filetypes = (("Text files",
                                                                                     "*.txt*"), ("all files","*.*")))
-    return filename
+    sys.argv.append(filename)
+    window.destroy()
 
 # Create the root window
-#window = tk.Tk()
+window = tk.Tk()
 
 # Set window title
-#window.title('File Explorer')
+window.title('File Explorer')
 
 # Set window size
-#window.geometry("500x300")
+window.geometry("500x300")
 
 # Set window background color
-#window.config(background="white")
+window.config(background="white")
 
 # Create a File Explorer label
-#label_file_explorer = tk.Label(window, text="Please select an input file", width=75, height=4, fg="blue")
+label_file_explorer = tk.Label(window, text="Please select an input file", width=75, height=4, fg="blue")
 
 #Creating buttons for functionality
-#button_files = tk.Button(window, text="Browse Files", command=browse_files)
-#button_exit = tk.Button(window, text="Exit", command=exit)
+button_files = tk.Button(window, text="Browse Files", command=browse_files)
+button_exit = tk.Button(window, text="Exit", command=exit)
 
 # Layout of objects on screen
-#label_file_explorer.grid(column=1, row=1)
-#button_files.grid(column=1, row=2)
-#button_exit.grid(column=1, row=3)
+label_file_explorer.grid(column=1, row=1)
+button_files.grid(column=1, row=2)
+button_exit.grid(column=1, row=3)
 
 # Let the window wait for user click
-#window.mainloop()
+window.mainloop()
